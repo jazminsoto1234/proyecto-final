59bef2d7f5d3cd41cbd3ea5ac75a3ad706dcc58d
Jazmin <jazmin.soto@utec.edu.pe>
2023-11-20 13:46:38 -0500

    Elimine demo2

--- demo2/src/main/java/com/example/demo/Person/domain/PersonRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.Person.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepository extends JpaRepository<Person, Long> {
-}
